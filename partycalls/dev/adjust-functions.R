712c1da253e40421f382834176da46f747e34cfd
Hershberger <hershberger.107@osu.edu>
2016-10-14 22:05:22 -0400

    Added function parameters and roll call data
    Function parameters can be found in adjust-functions.R in the dev
    folder.

--- partycalls/dev/adjust-functions.R
+++ partycalls/dev/adjust-functions.R
@@ -1,3 +1,5 @@
+library(partycalls)
+
 symdiff <- function(x, y)
 {
   sort(setdiff(base::union(x, y), base::intersect(x, y)))
@@ -7,7 +9,7 @@ test_rollcall <- function(.SD)
 {
   if (mean(.SD[, y], na.rm = TRUE) %in% c(0:1, NaN) |
       length(unique(.SD[!is.na(y) & party %in% c("D", "R"), party])) == 1L) {
-    list(b = 0, se = 0, t = Inf, p = NA_real_)
+    list(b = 0, se = 0, t = Inf, p = 0)
   } else {
     m <- lm(y ~ party + x, data = .SD)
     suppressWarnings(summ <- summary(m)$coef["partyR", ])
@@ -41,14 +43,13 @@ code_party_calls_1step <- function(rc, DT, noncalls)
   pvals <- regs$p
   pvals[is.na(pvals)] <- 1
   pvals
-  ok <- pvals > .01
-  which(ok)
+  # ok <- pvals > .01
+  # which(ok)
 }
 
-# TODO: make arguments for stopping points so we can adjust when we run
-# TODO: change switched vote indicator to <= 0.1%
-
-code_party_calls <- function(rc)
+code_party_calls <- function(rc, pval_threshold = 0.01, count_min = 15,
+  count_max = 150, match_count_min = 10, sim_annealing = TRUE,
+  random_seed = TRUE, lopside_thresh = 0.65, vote_switch_percent = 0.01)
 {
   rc <- pscl::dropRollCall(rc, dropList = alist(dropLegis = state == "USA"))
   rc <- emIRT::convertRC(rc, type = "binIRT")
@@ -57,22 +58,37 @@ code_party_calls <- function(rc)
   DT$party <- rc$legis.data$party
   DT[y %in% c(0, 9), y:= NA]
   DT[y == -1, y:= 0]
-  noncalls <- sample(rc$m, floor(.5 * rc$m))
+  if (random_seed == TRUE) {
+    noncalls <- sample(rc$m, floor(.5 * rc$m))
+  } else {
+    noncalls_DT <- DT[, yea_perc := mean(y, na.rm = TRUE), by = vt]
+    noncalls_DT <-
+      subset(DT, yea_perc < lopside_thresh & yea_perc > 1 - lopside_thresh,
+        select = vt)
+    noncalls_DT <- c(unique(noncall_DT$vt))
+    noncalls <-
+      as.numeric(c(gsub(pattern = "Vote ", replacement = "", noncalls_DT)))
+  }
   switched_votes <- seq_len(rc$m)
   match_counter <- 0
   counter <- 0
   record_of_coding <- list()
   record_of_pvals <- list()
-  while (counter <= 15 | (counter < 150 & match_counter < 10)) {
+  while (counter <= count_min |
+      (counter < count_max & match_counter < match_count_min)) {
     counter <- counter + 1
     record_of_coding[[counter]] <- noncalls
     old_noncalls <- noncalls
     old_switched_votes <- switched_votes
     pvals <- code_party_calls_1step(rc, DT, noncalls)
     record_of_pvals[[counter]] <- pvals
-    noncalls <- which(pvals > .05)
+    noncalls <- which(pvals > pval_threshold)
     calls <- setdiff(seq_len(rc$m), old_noncalls)
-    n_random_switches <- floor(rc$m * .2 * max(0, 1 - counter / 50) ^ 2)
+    if (sim_annealing = TRUE) {
+      n_random_switches <- floor(rc$m * .2 * max(0, 1 - counter / 50) ^ 2)
+    } else {
+      n_random_switches <- 0
+    }
     if (n_random_switches > 0) {
       calls_to_switch <- sample(calls, n_random_switches)
       noncalls_to_switch <- sample(noncalls, n_random_switches)
@@ -82,7 +98,7 @@ code_party_calls <- function(rc)
       noncalls <- c(noncalls_to_keep, calls_to_switch)
     }
     switched_votes <- symdiff(noncalls, old_noncalls)
-    if (length(switched_votes) <= 5) {
+    if (length(switched_votes) <= vote_switch_percent * rc$m) {
       match_counter <- match_counter + 1
     } else {
       match_counter <- 0
---
+++ partycalls/dev/learn-functions.R
@@ -0,0 +1,25 @@
+library(partycalls)
+library(data.table)
+options(stringsAsFactors = FALSE)
+load("inst/extdata/houKHfiles001-111.rdata")
+
+h111_test <- h111
+h111_test <- pscl::dropRollCall(h111_test,
+  dropList = alist(dropLegis = state == "USA"))
+h111_test <- emIRT::convertRC(h111_test, type = "binIRT")
+
+h111_test$votes
+
+DT_test <- CJ(vt = colnames(h111_test$votes), mc = rownames(h111_test$votes),
+  sorted = FALSE)
+DT_test$y <- as.vector(h111_test$votes)
+DT_test$party <- h111_test$legis.data$party
+DT_test[y %in% c(0, 9), y:= NA]
+DT_test[y == -1, y:= 0]
+View(DT_test)
+
+DT_test[, yea_perc := mean(y, na.rm = TRUE), by = vt]
+noncall_test <- subset(DT_test, yea_perc < 0.65 & yea_perc > 0.35, select = vt)
+noncall_test <- c(unique(noncall_test$vt))
+noncall_test <-
+  as.numeric(c(gsub(pattern = "Vote ", replacement = "", noncall_test)))
