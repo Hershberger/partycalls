4e7d164db85226c908f2d7a93b91238aa640f7b4
minozzi <william.minozzi@gmail.com>
2016-10-21 08:13:21 -0400

    comparison of emIRT replication of Who Heeds 13

--- partycalls/dev/code-house-party-calls-replication.R
+++ partycalls/dev/code-house-party-calls-replication.R
@@ -19,3 +19,34 @@ code_party_calls_by_congress_number <- function(congress_number)
 house_party_calls <- lapply(93:109, code_party_calls_by_congress_number)
 names(house_party_calls) <- paste0("hou", 93:109)
 save(house_party_calls, file = "inst/extdata/house_party_calls.RData")
+
+
+new_partycalls <- rbindlist(lapply(house_party_calls, function(x) data.table(
+    congress = gsub("[A-Za-z:/\\.]", "", x$source),
+    voteno = x$party_call_coding$voteno,
+    new_coding = x$party_call_coding$coding)))
+load("inst/extdata/votedata-partycalls.RData")
+setDT(votedata)
+old_partycalls <- votedata[congress < 110, .(congress, voteno, partycall)]
+old_partycalls[, old_coding := "gray"]
+old_partycalls[partycall == TRUE, old_coding := "party call"]
+old_partycalls[partycall == FALSE, old_coding := "noncall"]
+old_partycalls[, partycall := NULL]
+old_partycalls[, congress := as.character(congress)]
+old_partycalls[, voteno := as.character(voteno)]
+
+merged_votes <- merge(old_partycalls, new_partycalls,
+  by = c("congress", "voteno"), all = TRUE)
+tab1 <- merged_votes[, table(old_coding, new_coding)]
+tab2 <- merged_votes[congress != 104, table(old_coding, new_coding)]
+chisq.test(tab1)
+chisq.test(tab2)
+merged_votes[, chisq.test(table(old_coding, new_coding))$stat, congress]
+diag_prop <- function(x) sum(diag(x)) / sum(x)
+merged_votes[congress != 104, diag_prop(table(old_coding, new_coding))]
+merged_votes[congress != 104, diag_prop(table(old_coding, new_coding)), congress]
+merged_votes[congress == 102, table(old_coding, new_coding)]
+merged_votes[congress == 103, table(old_coding, new_coding)]
+merged_votes[congress == 109, table(old_coding, new_coding)]
+merged_votes[congress == 96, table(old_coding, new_coding)]
+merged_votes[congress == 98, table(old_coding, new_coding)]
