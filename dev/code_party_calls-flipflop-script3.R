639a8668d6159f9d9e20f9c9bb86fbaa1f74dc09
Hershberger <hershberger.107@osu.edu>
2016-11-08 14:07:31 -0500

    fix folder structure

--- dev/code_party_calls-flipflop-script3.R
+++ dev/code_party_calls-flipflop-script3.R
@@ -1,22 +1,7 @@
 library(data.table)
 options(stringsAsFactors = FALSE)
-source("dev/functions-improvement-attempt-5.R")
+source("dev/functions-improvement-attempt-3.R")
 load("inst/extdata/houKHfiles001-111.rdata")
-# load("inst/extdata/votedata-partycalls.RData")
-# setDT(votedata)
-# old_partycalls <- votedata[congress < 110, .(congress, voteno, partycall)]
-# old_partycalls[, old_coding := "gray"]
-# old_partycalls[partycall == TRUE, old_coding := "party call"]
-# old_partycalls[partycall == FALSE, old_coding := "noncall"]
-# old_partycalls[, partycall := NULL]
-# old_partycalls[, congress := as.character(congress)]
-# old_partycalls[, voteno := as.character(voteno)]
-# whoheeds13 <- readstata13::read.dta13(
-#   "inst/extdata/who-heeds-replication-archive.dta")
-# setDT(whoheeds13)
-# old_ideal_points <- whoheeds13[,
-#   .(congress, icpsr, dem, old_ideal_point = ideal_partyfree)]
-
 
 set.seed(1975242355)
 code_party_calls_by_congress_number <- function(congress_number)
@@ -25,23 +10,12 @@ code_party_calls_by_congress_number <- function(congress_number)
   rc <- get(paste0("h", sprintf("%03.f", congress_number)))
   rc <- code_party_calls(
     rc,
-    pval_threshold = 0.01,
-    count_min = 15,
-    count_max = 150,
-    match_count_min = 5,
-    sim_annealing = FALSE,
-    random_seed = FALSE,
-    lopside_thresh = 0.65,
-    drop_very_lopsided_votes = TRUE,
-    return_pvals = TRUE,
-    n_iterations_for_coding = 5,
-    use_new_match_check = TRUE,
-    type = "brglm",
-    temperature_method = "curve",
-    heat_time = 20,
-    cool_time = 40,
-    curve_mean = 10,
-    curve_sd = 15,
+    pval_threshold = 0.01, tval_threshold = 2.32, count_min = 15,
+    count_max = 150, match_count_min = 150, sim_annealing = TRUE,
+    random_seed = FALSE, lopside_thresh = 0.65, vote_switch_percent = 0.01,
+    drop_very_lopsided_votes = TRUE, return_pvals = FALSE,
+    n_iterations_for_coding = 5, use_new_match_check = TRUE,
+    type = c("brglm", "lm", "glm"), use_classification_distance = FALSE,
     remove_flip_flop_votes_from_noncalls = FALSE,
     randomly_reassign_flip_flop_votes_from_noncalls = TRUE)
 }
@@ -49,6 +23,5 @@ code_party_calls_by_congress_number <- function(congress_number)
 house_party_calls <- lapply(93:109, code_party_calls_by_congress_number)
 names(house_party_calls) <- paste0("hou", 93:109)
 save(house_party_calls,
-  file = "test_data/house_party_calls_flipflop.RData")
-
+  file = "test_data/house_party_calls_flipflop3.RData")
 
