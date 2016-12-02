bb770efcb4fc488a1be6c182048a82ceda0638ec
Hershberger <hershberger.107@osu.edu>
2016-12-02 16:32:23 -0500

    testing problem with senate 95 coding

---
+++ dev/senate_error_script.R
@@ -0,0 +1,9 @@
+library(data.table)
+library(partycalls)
+
+# throws error with any functions we have made so far so i'm just loading the
+# actual package for this
+
+load("inst/extdata/senate93-112.RData")
+
+senate_party_calls <- code_party_calls(sen95, sim_annealing = TRUE)
