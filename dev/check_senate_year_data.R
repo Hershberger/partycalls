a197ba0194ef9f1cf1fe98d1f11d651a37205dc1
Hershberger <hershberger.107@osu.edu>
2016-12-26 22:32:59 -0500

    begin correcting data

---
+++ dev/check_senate_year_data.R
@@ -0,0 +1,5 @@
+# load data
+
+# build senate year data with LES data and old methods
+
+# figure out what variables they differ on
---
+++ dev/standardize_ideal_points_senate_emIRT_only.R
@@ -0,0 +1,15 @@
+library(data.table)
+
+# load senator year data
+load("data/senator_year_data_emIRT_only.RData")
+
+# rescale ideal point mean to 0
+senator_year_data$ideological_extremism <-
+  senator_year_data$ideological_extremism - mean(senator_year_data$ideological_extremism)
+
+# rescale ideal point standard deviation to 1
+senator_year_data$ideological_extremism <-
+  senator_year_data$ideological_extremism / sd(senator_year_data$ideological_extremism)
+
+save(senator_year_data,
+  file = "data/senator_year_data_emIRT_only.RData")
