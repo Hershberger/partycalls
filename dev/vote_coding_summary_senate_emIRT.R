2081daa5099f92ccd04d7c53488ca1e8f8b48e4d
Hershberger <hershberger.107@osu.edu>
2017-01-11 12:51:56 -0500

    finish script for coding record

--- dev/vote_coding_summary_senate_emIRT.R
+++ dev/vote_coding_summary_senate_emIRT.R
@@ -4,7 +4,6 @@ load("test_data/senate_party_calls_emIRT_only.RData")
 names(senate_party_calls) <- paste0("sen", 93:112)
 
 senate_coding_record <- data.frame(row.names = 93:112)
-senate_coding_record$congress <- 93:112
 senate_coding_record$party_call_count <- sapply(93:112, function(x)
   length(get_party_calls(senate_party_calls[[paste0("sen", x)]])))
 senate_coding_record$noncall_count <- sapply(93:112, function(x)
