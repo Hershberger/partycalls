5b22270cea283ca9b32124cecf2b57282d5de32b
Hershberger <hershberger.107@osu.edu>
2017-01-17 20:11:48 -0500

    work on house replication

--- dev/house_party_calls_table3_emIRT_only.R
+++ dev/house_party_calls_table3_emIRT_only.R
@@ -5,13 +5,26 @@ options(stringsAsFactors = FALSE)
 
 # load data for analysis
 load("test_data/new_whoheeds13_emIRT_only.RData")
-# whoheeds13 <- readstata13::read.dta13(
-#   "inst/extdata/who-heeds-replication-archive.dta")
-# setDT(whoheeds13)
-# whoheeds13 <- whoheeds13[, .(
-#   congress, icpsr, maj, retiree, bestgrosswart, dpres
-# )]
-# new_whoheeds13 <- merge(new_whoheeds13, whoheeds13, by = c("congress", "icpsr"))
+whoheeds13 <- readstata13::read.dta13(
+  "inst/extdata/who-heeds-replication-archive.dta")
+setDT(whoheeds13)
+whoheeds13 <- whoheeds13[, .(
+  congress, icpsr, maj, retiree, bestgrosswart
+)]
+
+setnames(whoheeds13, "icpsr", "icpsrLegis")
+best_committee_old <- whoheeds13[, .(congress, icpsrLegis)]
+
+bestgrosswart_dt <- fread("inst/extdata/house_assignments_103-114-1.csv")
+setnames(bestgrosswart_dt, "Congress", "congress")
+setnames(bestgrosswart_dt, "ID #", "icpsrLegis")
+bestgrosswart_dt <- bestgrosswart_dt[congress <= 112,
+  .(bestgrosswart = min(rank, na.rm = TRUE)),
+  .(congress, icpsrLegis, Name)]
+
+new_whoheeds13 <- merge(new_whoheeds13, whoheeds13, by = c("congress", "icpsr"))
+
+
 
 f_extremism <- pirate100 ~ ideological_extremism +
   pfrate100 + pres_votepct + south + votepct + female + afam + latino +
