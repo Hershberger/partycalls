d716be15870b7262ec04a8dc62d450c9dd9cbc21
Hershberger <hershberger.107@osu.edu>
2017-01-17 23:25:42 -0500

    get corrected senate summary stats

--- dev/senate_data_summary_stats_emIRT.R
+++ dev/senate_data_summary_stats_emIRT.R
@@ -1,10 +1,28 @@
 library(partycalls)
-library(stargazer)
 
 load("test_data/senate_data_emIRT_only.RData")
+setDT(senate_data)
 
-senate_data_dem <- senate_data[caucus == "Democrat",]
-senate_data_rep <- senate_data[caucus == "Republican", ]
+senate_data <- senate_data[, .(congress, class, pres_vote_share, votepct,
+  south, leader, chair, best_committee, power_committee, up_for_reelection,
+  freshman, seniority, retiree, afam, latino, female, maj, caucus,
+  party_free_ideal_point, pirate100, pfrate100, ideological_extremism,
+  icpsrLegis)]
 
-stargazer(senate_data_dem, summary = TRUE, summary.stat = c("mean", "sd"))
-stargazer(senate_data_rep, summary = TRUE, summary.stat = c("mean", "sd"))
+# correct buckley extremism
+senate_data[icpsrLegis == 13100, ideological_extremism := abs(party_free_ideal_point)]
+
+senate_dem <- senate_data[caucus == "Democrat", ]
+senate_rep <- senate_data[caucus == "Republican", ]
+senate_maj <- senate_data[maj == 1, ]
+senate_min <- senate_data[maj == 0, ]
+
+# weicker listed as chair in majority party, congress 100
+senate_min[chair == 1, icpsrLegis, congress]
+senate_min[icpsrLegis == 12032, chair := 0]
+
+stargazer::stargazer(senate_dem, summary = TRUE)
+stargazer::stargazer(senate_rep, summary = TRUE)
+stargazer::stargazer(senate_maj, summary = TRUE)
+stargazer::stargazer(senate_min, summary = TRUE)
+
