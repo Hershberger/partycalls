94297c8e148c6c4ddb6f33ec64d008289fc690e1
Hershberger <hershberger.107@osu.edu>
2017-02-13 15:47:56 -0500

    make senate iv/dv and dv/dv plots and tables

---
+++ dev/dv_dv_plot_senate_southdem_gingrichsen.R
@@ -0,0 +1,50 @@
+library(partycalls)
+library(ggplot2)
+
+theme_set(theme_bw())
+
+load("test_data/senate_data_p_05.RData")
+
+senate_data <- senate_data[drop == 0, ]
+
+senate_data[, south := as.factor(south)]
+senate_data[, majority := as.factor(maj)]
+senate_data[, gingrich_senator := as.factor(gingrich_senator)]
+
+senate_dem <- senate_data[caucus == "Democrat", ]
+senate_rep <- senate_data[caucus == "Republican", ]
+
+# save as plots/senate_p_05_dem_dv-dv_2_south.pdf
+ggplot(senate_dem, aes(ideological_extremism, pfrate100, color = south)) +
+  geom_point(shape = 16) +
+  scale_color_manual(breaks = c("0", "1"),
+    values = c("blue2", "gray25")) +
+  geom_smooth(method=loess, se=FALSE)
+
+# save as plots/senate_p_05_dem_dv-dv_2_majority.pdf
+ggplot(senate_dem, aes(ideological_extremism, pfrate100, color = majority)) +
+  geom_point(shape = 16) +
+  scale_color_manual(breaks = c("0", "0.75", "1"),
+    values = c("blue2", "gray55", "gray25")) +
+  geom_smooth(method=loess, se=FALSE)
+
+# save as plots/senate_p_05_rep_dv-dv_2_gingrich.pdf
+ggplot(senate_rep, aes(ideological_extremism, pfrate100, color = gingrich_senator)) +
+  geom_point(shape = 16) +
+  scale_color_manual(breaks = c("0", "1"),
+      values = c("red2", "gray25")) +
+  geom_smooth(method=loess, se=FALSE)
+
+# save as plots/senate_p_05_rep_dv-dv_2_majority.pdf
+ggplot(senate_rep, aes(ideological_extremism, pfrate100, color = majority)) +
+  geom_point(shape = 16) +
+  scale_color_manual(breaks = c("0", "0.75", "1"),
+    values = c("red2", "gray55", "gray25")) +
+  geom_smooth(method=loess, se=FALSE)
+
+# save as plots/senate_p_05_rep_dv-dv_2_south.pdf
+ggplot(senate_rep, aes(ideological_extremism, pfrate100, color = south)) +
+  geom_point(shape = 16) +
+  scale_color_manual(breaks = c("0", "1"),
+    values = c("red2", "gray25")) +
+  geom_smooth(method=loess, se=FALSE)
---
+++ dev/senate_data_summary_stats_keep_very_lopsided.R
@@ -0,0 +1,15 @@
+library(partycalls)
+load("test_data/senate_data_keep_very_lopsided.RData")
+
+senate_data <- senate_data[drop == 0,]
+
+senate_dem <- senate_data[caucus == "Democrat", ]
+senate_rep <- senate_data[caucus == "Republican", ]
+senate_maj <- senate_data[maj == 1, ]
+senate_min <- senate_data[maj == 0, ]
+
+stargazer::stargazer(senate_data, summary = TRUE)
+stargazer::stargazer(senate_dem, summary = TRUE)
+stargazer::stargazer(senate_rep, summary = TRUE)
+stargazer::stargazer(senate_maj, summary = TRUE)
+stargazer::stargazer(senate_min, summary = TRUE)
