9e082a81209c1dd10efdb42b6130b8967b61206f
Hershberger <hershberger.107@osu.edu>
2017-03-05 13:54:57 -0500

    variable tests

---
+++ dev/senate_lm_inter_party_table.R
@@ -0,0 +1,28 @@
+library(partycalls)
+library(texreg)
+library(data.table)
+options(stringsAsFactors = FALSE)
+
+# load data for analysis
+load("test_data/senate_data_lm.RData")
+senate_data <- senate_data[drop == 0, ]
+
+f_extremism <- pirate100 ~ ideological_extremism +
+  pfrate100 + pres_vote_share + south + vote_share +
+  female + afam + latino + up_for_reelection +
+  seniority + freshman + retiree + best_committee + leader +
+  power_committee + chair
+
+texreg::screenreg(list(lm(f_extremism, senate_data[caucus == "Democrat" ]),
+  lm(f_extremism, senate_data[caucus == "Republican"]),
+  lm(f_extremism, senate_data[maj == 1]),
+  lm(f_extremism, senate_data[maj == 0])),
+  reorder.coef = c(2, 17, 3:5, 16, 6:15, 1),
+  digits = 3)
+
+texreg::texreg(list(lm(f_extremism, senate_data[caucus == "Democrat"]),
+  lm(f_extremism, senate_data[caucus == "Republican"]),
+  lm(f_extremism, senate_data[maj == 1]),
+  lm(f_extremism, senate_data[maj == 0])),
+  reorder.coef = c(2, 17, 3:5, 16, 6:15, 1),
+  digits = 3)
