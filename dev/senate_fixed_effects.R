ca6da645c5ee00382f44e34e833e4b8a9e99b3ee
Hershberger <hershberger.107@osu.edu>
2017-03-05 12:48:59 -0500

    make fixed effects model script

---
+++ dev/senate_fixed_effects.R
@@ -0,0 +1,28 @@
+library(partycalls)
+library(lfe)
+
+load("test_data/senate_data_lm.RData")
+
+senate_data <- senate_data[drop == 0, ]
+
+mod1 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+  pres_vote_share + freshman + retiree  + best_committee + up_for_reelection +
+  power_committee + leader + chair | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[caucus == "Democrat"])
+
+mod2 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+  pres_vote_share + freshman + retiree + best_committee + up_for_reelection +
+  power_committee + leader + chair | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[caucus == "Republican"])
+
+mod3 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+  pres_vote_share + freshman + retiree + best_committee + up_for_reelection +
+  power_committee + leader + chair | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[maj == 1])
+
+mod4 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+  pres_vote_share + freshman + retiree + best_committee + up_for_reelection +
+  power_committee + leader | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[maj == 0])
+
+texreg::screenreg(list(mod1, mod2, mod3, mod4))
