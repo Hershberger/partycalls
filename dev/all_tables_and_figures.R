23f7f51ed18b27ac070fefb27b4b8557511dcbf1
Hershberger <hershberger.107@osu.edu>
2017-03-28 13:38:30 -0400

    finish weekly update

--- dev/all_tables_and_figures.R
+++ dev/all_tables_and_figures.R
@@ -13,8 +13,33 @@ senate_data[congress == 107 & caucus == "Republican", maj := 0]
 senate_data[congress == 107 & caucus == "Democrat", maj := 1]
 senate_data[, vote_share := vote_share * 100]
 senate_data[, pres_vote_share := pres_vote_share * 100]
+load("test_data/house_party_calls_lm.RData")
+names(house_party_calls) <- paste0("hou", 93:112)
+load("test_data/senate_party_calls_lm.RData")
+names(senate_party_calls) <- paste0("sen", 93:112)
 
 # subsets of the data
+house_coding_record <- data.table(congress = 93:112)
+house_coding_record[, party_calls := sapply(93:112, function(x)
+  length(get_party_calls(house_party_calls[[paste0("hou", x)]])))]
+house_coding_record[, noncalls := sapply(93:112, function(x)
+  length(get_noncalls(house_party_calls[[paste0("hou", x)]])))]
+house_coding_record[, gray_vote_count := sapply(93:112, function(x)
+  length(get_gray_votes(house_party_calls[[paste0("hou", x)]])))]
+house_coding_record[, percent_party_calls :=
+    100 * party_calls / (party_calls + noncalls)]
+
+senate_coding_record <- data.table(congress = 93:112)
+senate_coding_record[, party_calls := sapply(93:112, function(x)
+  length(get_party_calls(senate_party_calls[[paste0("sen", x)]])))]
+senate_coding_record[, noncalls := sapply(93:112, function(x)
+  length(get_noncalls(senate_party_calls[[paste0("sen", x)]])))]
+senate_coding_record[, gray_vote_count := sapply(93:112, function(x)
+  length(get_gray_votes(senate_party_calls[[paste0("sen", x)]])))]
+senate_coding_record[, percent_party_calls :=
+    100 * party_calls / (party_calls + noncalls)]
+
+
 DATA <- senate_data[!is.na(pirate100), .(congress, stabb, class, caucus, maj,
   tr = up_for_reelection, y = pirate100 - pfrate, y1 = pirate100, y2 = pfrate100)]
 setorder(DATA, stabb, congress, class)
@@ -266,8 +291,27 @@ texreg::texreg(list(lm(sen_extremism, senate_data[caucus == "Democrat"]),
   lm(sen_extremism, senate_data[maj == 0])),
   reorder.coef = c(2:3, 10, 6, 4, 5, 7:9, 11:17, 1))
 
-
 # Make Figure 3
+hou_record_plot <- ggplot(house_coding_record, aes(congress, percent_party_calls)) +
+  ylim(30, 90) +
+  xlab("Congress") +
+  ylab("Party Call Percent") +
+  labs(title = "Party Call Percent in the House") +
+  geom_point(color = "red2") +
+  geom_line() +
+  theme_classic()
+sen_record_plot <- ggplot(senate_coding_record, aes(congress, percent_party_calls)) +
+  ylim(30, 90) +
+  xlab("Congress") +
+  ylab("Party Call Percent") +
+  labs(title = "Party Call Percent in the Senate") +
+  geom_point(color = "red2") +
+  geom_line() +
+  theme_classic()
+fig3 <- arrangeGrob(hou_record_plot, sen_record_plot, ncol = 2)
+ggsave("plots/party_call_percent_both.pdf", fig3)
+
+# Make Figure 4
 set.seed(98037298)
 DATA[, rand := runif(nrow(DATA))]
 
@@ -362,7 +406,7 @@ segments(naive_difference1$placement, naive_difference1$Lower_Bound,
 dev.off()
 
 
-# Make Figure 4
+# Make Figure 5
 set.seed(98037298)
 
 boot <- function(i) {
@@ -448,7 +492,52 @@ xtable(coef_signs)
 
 
 # Make Tables 7 & 8
-
+naive_difference1[, placement := NULL]
+seat_type_effect <- DATA[mean_tr == .5, sum(tr * y1) - sum((1 - tr) * y1),
+  .(stabb, congress, seat_pair_type)][, mean(V1), .(seat_pair_type)]
+setnames(seat_type_effect, "V1", "effect")
+seat_type_placebo <- DATA[, sum((rand > mean(rand)) * y1) -
+    sum(((rand) <= mean(rand)) * y1),
+  .(stabb, congress, seat_pair_type)][, mean(V1), .(seat_pair_type)]
+setnames(seat_type_placebo, "V1", "placebo")
+seat_type_difference <- data.table(
+  Test = c("2 Maj Dems Effect", "2 Maj Dems Placebo",
+    "2 Min Dems Effect", "2 Min Dems Placebo",
+    "2 Maj Reps Effect", "2 Maj Reps Placebo",
+    "2 Min Reps Effect", "2 Min Reps Placebo",
+    "Split, Maj Dem, Dem Effect", "Split, Maj Dem, Dem Placebo",
+    "Split, Maj Dem, Rep Effect", "Split, Maj Dem, Rep Placebo",
+    "Split, Maj Rep, Dem Effect", "Split, Maj Rep, Dem Placebo",
+    "Split, Maj Rep, Rep Effect", "Split, Maj Rep, Rep Placebo"),
+  DV = "pirate100",
+  Estimate = c(
+    seat_type_effect[seat_pair_type == "2 maj dems", effect],
+    seat_type_placebo[seat_pair_type == "2 maj dems", placebo],
+    seat_type_effect[seat_pair_type == "2 min dems", effect],
+    seat_type_placebo[seat_pair_type == "2 min dems", placebo],
+    seat_type_effect[seat_pair_type == "2 maj reps", effect],
+    seat_type_placebo[seat_pair_type == "2 maj reps", placebo],
+    seat_type_effect[seat_pair_type == "2 min reps", effect],
+    seat_type_placebo[seat_pair_type == "2 min reps", placebo],
+    seat_type_effect[seat_pair_type == "split/maj dem, dem", effect],
+    seat_type_placebo[seat_pair_type == "split/maj dem, dem", placebo],
+    seat_type_effect[seat_pair_type == "split/maj dem, rep", effect],
+    seat_type_placebo[seat_pair_type == "split/maj dem, rep", placebo],
+    seat_type_effect[seat_pair_type == "split/maj rep, dem", effect],
+    seat_type_placebo[seat_pair_type == "split/maj rep, dem", placebo],
+    seat_type_effect[seat_pair_type == "split/maj rep, rep", effect],
+    seat_type_placebo[seat_pair_type == "split/maj rep, rep", placebo]
+  )
+)
+naive_difference1_tex <- xtable(naive_difference1, auto = TRUE,
+  caption = "Reelection and Response to Party Calls, Difference in Differences",
+  digits = c(3, 3, 3, 3, 3, 3))
+print(naive_difference1_tex, include.rownames = FALSE,
+  table.placement = "H", caption.placement = "top")
+seat_type_difference_tex <- xtable(seat_type_difference, auto = TRUE,
+  caption = "Diff in Diff, Subgroup Condition, Party Influenced Rate")
+print(seat_type_difference_tex, include.rownames = FALSE,
+  table.placement = "H", caption.placement = "top")
 
 
 # Make Tables 9 & 10
@@ -518,3 +607,140 @@ seat_type_difference_tex <- xtable(seat_type_difference, auto = TRUE,
   caption = "Diff in Diff, Subgroup Condition, Party Influenced Rate")
 print(seat_type_difference_tex, include.rownames = FALSE,
   table.placement = "H", caption.placement = "top")
+
+
+# Make Table 11
+mod1 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+    pres_vote_share + freshman + retiree  + best_committee + up_for_reelection +
+    power_committee + leader + chair | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[caucus == "Democrat"])
+mod2 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+    pres_vote_share + freshman + retiree + best_committee + up_for_reelection +
+    power_committee + leader + chair | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[caucus == "Republican"])
+mod3 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+    pres_vote_share + freshman + retiree + best_committee + up_for_reelection +
+    power_committee + leader + chair | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[maj == 1])
+mod4 <- felm(pirate100 ~ ideological_extremism + pfrate100 + vote_share +
+    pres_vote_share + freshman + retiree + best_committee + up_for_reelection +
+    power_committee + leader | icpsrLegis + congress | 0 | icpsrLegis,
+  senate_data[maj == 0])
+texreg::texreg(list(mod1, mod2, mod3, mod4),
+  reorder.coef = c(1:2, 8, 3:7, 9:11))
+
+
+# Make Table 12
+texreg::texreg(list(
+  lm(hou_extremism, new_whoheeds13[dem == 1 & congress == 97]),
+  lm(hou_extremism, new_whoheeds13[dem == 1 & congress == 102]),
+  lm(hou_extremism, new_whoheeds13[dem == 1 & congress == 107]),
+  lm(hou_extremism, new_whoheeds13[dem == 0 & congress == 97]),
+  lm(hou_extremism, new_whoheeds13[dem == 0 & congress == 102]),
+  lm(hou_extremism, new_whoheeds13[dem == 0 & congress == 107])
+), reorder.coef = c(2:15, 1))
+
+
+# Make Figure 6
+fm_extremism_hou <- function(i, j) {
+  summary(lm(hou_extremism,
+    data = subset(new_whoheeds13, congress == i & majority == j)),
+    vcov = vcovHC(type = "HC1"))
+}
+B_hou <- SE_hou <- data.frame(row.names = 93:112)
+B_hou$extremism_maj_hou <- sapply(93:112, function(x)
+  fm_extremism_hou(x, 1)$coef["ideological_extremism", "Estimate"])
+B_hou$extremism_min_hou <- sapply(93:112, function(x)
+  fm_extremism_hou(x, 0)$coef["ideological_extremism", "Estimate"])
+SE_hou$extremism_maj_hou <- sapply(93:112, function(x)
+  fm_extremism_hou(x, 1)$coef["ideological_extremism", "Std. Error"])
+SE_hou$extremism_min_hou <- sapply(93:112, function(x)
+  fm_extremism_hou(x, 0)$coef["ideological_extremism", "Std. Error"])
+
+pdf(file="plots/who-heeds-figure2-replication_lm.pdf",
+  width = 4, height = 8, family = "Times")
+layout(matrix(1:2, 2, 1, byrow = TRUE))
+par(mar = c(2.5, 4, 2, 0.3) + 0.1, font.lab = 2)
+
+x_hou <- (93:112)
+x.ticks_hou <- c(94, 99, 104, 109)
+y.ticks_hou <- c(-12, 0, 12)
+
+b_hou <- B$extremism_maj_hou
+se_hou <- SE$extremism_maj_hou
+plot(0, 0, type='n', ylim=c(-4, 20), xlim=c(93, 112),
+  cex.lab=1.15, xaxt="n", yaxt="n", xlab="", ylab="Majority")
+axis(1, x.ticks, cex.axis=1.1, labels=TRUE)
+axis(2, y.ticks, cex.axis=1.1, labels=TRUE)
+abline(h=0, col="gray", xpd=FALSE)
+title(main="Ideological Extremism", cex.main=1.15, line=0.75, font.main=2)
+points(x_hou, b_hou, pch=19, col="black", cex=.8)
+segments(x_hou, b_hou - qnorm(.750) * se, x_hou, b+qnorm(.750)*se, lwd=2)
+segments(x_hou, b_hou - qnorm(.975) * se, x_hou, b+qnorm(.975)*se, lwd=.9)
+
+b_hou <- B$extremism_min_hou
+se_hou <- SE$extremism_min_hou
+plot(0, 0, type='n', ylim=c(-4, 20), xlim=c(93, 112),
+  cex.lab=1.15, xaxt="n", yaxt="n", xlab="", ylab="Minority")
+axis(1, x.ticks, cex.axis=1.1, labels=TRUE, xpd=TRUE)
+axis(2, y.ticks, cex.axis=1.1, labels=TRUE)
+abline(h=0, col="gray", xpd=FALSE)
+title(main="", cex.main=1.15, line=0.75, font.main=2)
+points(x_hou, b_hou, pch=19, col="black", cex=.8)
+segments(x_hou, b_hou - qnorm(.750)*se, x_hou, b+qnorm(.750)*se, lwd=2)
+segments(x_hou, b_hou - qnorm(.975)*se, x_hou, b+qnorm(.975)*se, lwd=.9)
+
+dev.off()
+
+
+# Make Figure 7
+fm_extremism <- function(i, j) {
+  summary(lm(sen_extremism,
+    data = subset(senate_data, congress == i & maj == j)),
+    vcov = vcovHC(type = "HC1"))
+}
+
+B <- SE <- data.frame(row.names = 93:112)
+B$extremism_maj <- sapply(93:112, function(x)
+  fm_extremism(x, 1)$coef["ideological_extremism", "Estimate"])
+B$extremism_min <- sapply(93:112, function(x)
+  fm_extremism(x, 0)$coef["ideological_extremism", "Estimate"])
+SE$extremism_maj <- sapply(93:112, function(x)
+  fm_extremism(x, 1)$coef["ideological_extremism", "Std. Error"])
+SE$extremism_min <- sapply(93:112, function(x)
+  fm_extremism(x, 0)$coef["ideological_extremism", "Std. Error"])
+
+pdf(file="plots/senate-figure2-lm.pdf",
+  width = 4, height = 8, family = "Times")
+layout(matrix(1:2, 2, 1, byrow = TRUE))
+par(mar = c(2.5, 4, 2, 0.3) + 0.1, font.lab = 2)
+
+x <- (93:112)
+x.ticks <- c(95, 100, 105, 110)
+y.ticks <- c(- 24, -12, -6, 0, 12)
+
+b <- B$extremism_maj#[-15]
+se <- SE$extremism_maj#[-15]
+plot(0, 0, type='n', ylim=c(-8, 24), xlim=c(93, 112),
+  cex.lab=1.15, xaxt="n", yaxt="n", xlab="", ylab="Majority Party")
+axis(1, x.ticks, cex.axis=1.1, labels=TRUE)
+axis(2, y.ticks, cex.axis=1.1, labels=TRUE)
+abline(h=0, col="gray", xpd=FALSE)
+title(main="Ideological Extremism", cex.main=1.15, line=0.75, font.main=2)
+points(x, b, pch=19, col="black", cex=.8)
+segments(x, b - qnorm(.750) * se, x, b+qnorm(.750)*se, lwd=2)
+segments(x, b - qnorm(.975) * se, x, b+qnorm(.975)*se, lwd=.9)
+
+b <- B$extremism_min#[-15]
+se <- SE$extremism_min#[-15]
+plot(0, 0, type='n', ylim=c(-8, 24), xlim=c(93, 112),
+  cex.lab=1.15, xaxt="n", yaxt="n", xlab="", ylab="Minority Party")
+axis(1, x.ticks, cex.axis=1.1, labels=TRUE, xpd=TRUE)
+axis(2, y.ticks, cex.axis=1.1, labels=TRUE)
+abline(h=0, col="gray", xpd=FALSE)
+title(main="", cex.main=1.15, line=0.75, font.main=2)
+points(x, b, pch=19, col="black", cex=.8)
+segments(x, b - qnorm(.750)*se, x, b+qnorm(.750)*se, lwd=2)
+segments(x, b - qnorm(.975)*se, x, b+qnorm(.975)*se, lwd=.9)
+
+dev.off()
