e940512b7d7df831df61b37db106090a8589a2a1
Hershberger <hershberger.107@osu.edu>
2017-05-24 10:46:04 -0400

    weekly update

---
+++ dev/pairs_diff_in_diff_no_placebo.R
@@ -0,0 +1,106 @@
+library(partycalls)
+library(xtable)
+set.seed(1339367524)
+load("test_data/senate_data_lm.RData")
+
+# select variables needed
+DATA <- senate_data[!is.na(pirate100), .(congress, stabb, class, caucus, maj,
+  votes, tr = up_for_reelection, y1 = pirate100, y2 = pfrate100)]
+setorder(DATA, stabb, congress, class)
+
+# make dems majority for congress 107
+DATA[congress == 107 & caucus == "Democrat", maj := 1]
+DATA[congress == 107 & caucus == "Republican", maj := 0]
+
+# subset to cases with two senators, one treated, one control
+DATA <- merge(DATA,
+  DATA[, .N, .(stabb, congress)],
+  by = c("stabb", "congress"),
+  all.x = TRUE)
+DATA <- DATA[N == 2]
+DATA[, mean_tr := mean(tr), .(stabb, congress)]
+
+DATA[, y := y1 - y2]
+
+# Estimate Effects
+diff_pi <- DATA[mean_tr == .5,
+  sum(tr * y1) - sum((1 - tr) * y1), .(stabb, congress)][,
+    mean(V1)]
+diff_pf <- DATA[mean_tr == .5,
+  sum(tr * y2) - sum((1 - tr) * y2), .(stabb, congress)][,
+    mean(V1)]
+diff_in <- DATA[mean_tr == .5,
+  sum(tr * y) - sum((1 - tr) * y), .(stabb, congress)][,
+    mean(V1)]
+
+# Do inference
+# bootstrap by state
+states <- DATA[, unique(stabb)]
+
+boot <- function(i) {
+  boot_states <- sample(states, replace = TRUE)
+  boot_DATA <- rbindlist(lapply(seq_along(boot_states), function(boot_id) {
+    boot_DATA <- DATA[stabb == boot_states[boot_id]]
+    boot_DATA[, boot_id := boot_id]
+    boot_DATA
+  }))
+  boot_diff_pi <- boot_DATA[mean_tr == .5,
+    sum(tr * y1) - sum((1 - tr) * y1), .(boot_id, congress)][,
+      mean(V1)]
+  boot_diff_pf <- boot_DATA[mean_tr == .5,
+    sum(tr * y2) - sum((1 - tr) * y2), .(boot_id, congress)][,
+      mean(V1)]
+  boot_diff_in <- boot_DATA[mean_tr == .5,
+    sum(tr * y) - sum((1 - tr) * y), .(boot_id, congress)][,
+      mean(V1)]
+  data.table(boot_diff_pi, boot_diff_pf, boot_diff_in)
+}
+
+boots <- rbindlist(lapply(1:1000, boot))
+differences <- data.table(test = c("Party Call Difference",
+  "Party Free Difference", "Difference in Differences"),
+  Estimate = c(diff_pi, diff_pf, diff_in),
+  Lower_Bound = c(boots[, quantile(boot_diff_pi, .025)],
+    boots[, quantile(boot_diff_pf, .025)],
+    boots[, quantile(boot_diff_in, .025)]),
+  Upper_Bound = c(boots[, quantile(boot_diff_pi, .975)],
+    boots[, quantile(boot_diff_pf, .975)],
+    boots[, quantile(boot_diff_in, .975)])
+)
+
+difference_tex <- xtable(differences, auto = TRUE,
+  caption = "Reelection and Response to Party Calls, Same-State Senator Differences",
+  digits = c(3, 3, 3, 3, 3, 3))
+print(difference_tex, include.rownames = FALSE,
+  table.placement = "H", caption.placement = "top")
+
+# make coeff plot from effects
+differences[, position := 0]
+differences[test == "Party Free Difference", position := 1]
+differences[test == "Difference in Differences", position := 2]
+differences[, Lower_Bound_50 := c(boots[, quantile(boot_diff_pi, .25)],
+  boots[, quantile(boot_diff_pf, .25)],
+  boots[, quantile(boot_diff_in, .25)])]
+differences[, Upper_Bound_50 := c(boots[, quantile(boot_diff_pi, .75)],
+  boots[, quantile(boot_diff_pf, .75)],
+  boots[, quantile(boot_diff_in, .75)])]
+
+pdf(file="plots/senate_difference_estimates.pdf", ## RENAME
+  width = 5, height = 4, family = "Times")
+
+plot(0, 0, type='n', ylim=c(-2.1, .2), xlim=c(-0.5, 2.5),
+  cex.lab=1.15, xaxt="n", yaxt="n", xlab="", ylab="Effect")
+axis(1, differences$position, cex.axis =.5,
+  labels = c("Party Call Rate", "Baseline", "Difference-in-Diffferences"))
+axis(2, c(-2, -1.5, -1, -0.5, 0), cex.axis = 1.1, labels = TRUE)
+abline(h=0, col="gray55", xpd=FALSE)
+title(main="Same-State Pair Differences, Reelection Treatment",
+  cex.main=.8, line=0.75, font.main=2)
+points(differences$position, differences$Estimate,
+  pch=19, col="black", cex=.8)
+segments(differences$position, differences$Lower_Bound_50,
+  differences$position,  differences$Upper_Bound_50, lwd = 2.5)
+segments(differences$position, differences$Lower_Bound,
+  differences$position,  differences$Upper_Bound, lwd = 1)
+
+dev.off()
--- dev/senate_summary_stats_lm.R
+++ dev/senate_summary_stats_lm.R
@@ -3,6 +3,11 @@ load("test_data/senate_data_lm.RData")
 
 senate_data <- senate_data[drop == 0,]
 
+senate_data <- senate_data[, .(party_free_ideal_point, ideological_extremism,
+  pirate100, pfrate100, up_for_reelection, retiree, vote_share, pres_vote_share,
+  leader, chair, power_committee, best_committee, female, afam, latino, south,
+  seniority, freshman, maj, caucus)]
+
 senate_dem <- senate_data[caucus == "Democrat", ]
 senate_rep <- senate_data[caucus == "Republican", ]
 senate_maj <- senate_data[maj == 1, ]
