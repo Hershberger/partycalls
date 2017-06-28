ce1b1eece07d28f00339f20df53ed562afa98ce8
minozzi <william.minozzi@gmail.com>
2017-06-28 13:50:15 -0400

    new versions of figures 1 and 2

--- dev/table-1.R
+++ dev/table-1.R
@@ -1,8 +1,23 @@
 library(partycalls)
+library(ggplot2)
+library(extrafont)
+library(Cairo)
+loadfonts()
 
+#------------------------------------------------------------------------------#
 # Load data
+#------------------------------------------------------------------------------#
 load("test_data/new_whoheeds13_lm.RData")
 house_data <- new_whoheeds13[drop == 0, ]
+load("test_data/senate_data_lm.RData")
+load("test_data/house_party_calls_lm.RData")
+names(house_party_calls) <- paste0("hou", 93:112)
+load("test_data/senate_party_calls_lm.RData")
+names(senate_party_calls) <- paste0("sen", 93:112)
+
+#------------------------------------------------------------------------------#
+# Cleaning (move this to another script)
+#------------------------------------------------------------------------------#
 house_data[is.na(vote_share), vote_share := 100]
 setnames(house_data,
   c("bestgrosswart", "power"),
@@ -12,7 +27,6 @@ new_names <- c("responsiveness_to_party_calls", "baseline_rate",
   "african_american")
 setnames(house_data, old_names, new_names)
 
-load("test_data/senate_data_lm.RData")
 senate_data <- senate_data[drop == 0, ]
 senate_data[, dem := NA_real_]
 senate_data[caucus == "Democrat", dem := 1]
@@ -23,9 +37,43 @@ setnames(senate_data,
   c("maj"),
   c("majority"))
 setnames(senate_data, old_names, new_names)
+senate_data[congress == 107, majority := dem]
 
-# analysis
+# drop Jeffords from 107?
+senate_data <- senate_data[!(congress == 107 & icpsrLegis %in% c(14240, 94240))]
 
+house_coding_record <- data.table(congress = 93:112)
+house_coding_record[, party_calls := sapply(93:112, function(x)
+  length(get_party_calls(house_party_calls[[paste0("hou", x)]])))]
+house_coding_record[, noncalls := sapply(93:112, function(x)
+  length(get_noncalls(house_party_calls[[paste0("hou", x)]])))]
+house_coding_record[, gray_vote_count := sapply(93:112, function(x)
+  length(get_gray_votes(house_party_calls[[paste0("hou", x)]])))]
+house_coding_record[, percent_party_calls :=
+    100 * party_calls / (party_calls + noncalls)]
+house_coding_record[, majority := "Republican"]
+house_coding_record[congress %in% c(93:103, 110:111), majority := "Democrat"]
+
+senate_coding_record <- data.table(congress = 93:112)
+senate_coding_record[, party_calls := sapply(93:112, function(x)
+  length(get_party_calls(senate_party_calls[[paste0("sen", x)]])))]
+senate_coding_record[, noncalls := sapply(93:112, function(x)
+  length(get_noncalls(senate_party_calls[[paste0("sen", x)]])))]
+senate_coding_record[, gray_vote_count := sapply(93:112, function(x)
+  length(get_gray_votes(senate_party_calls[[paste0("sen", x)]])))]
+senate_coding_record[, percent_party_calls :=
+    100 * party_calls / (party_calls + noncalls)]
+senate_coding_record[, majority := "Republican"]
+senate_coding_record[congress %in% c(93:96, 100:103, 107, 110:112),
+  majority := "Democrat"]
+
+house_coding_record[, chamber := "House"]
+senate_coding_record[, chamber := "Senate"]
+coding_record <- rbind(house_coding_record, senate_coding_record)
+
+#------------------------------------------------------------------------------#
+# models
+#------------------------------------------------------------------------------#
 formula1 <- responsiveness_to_party_calls ~
   ideological_extremism + baseline_rate +
   vote_share + pres_vote_share + leader +  chair + power_committee +
@@ -33,7 +81,9 @@ formula1 <- responsiveness_to_party_calls ~
   seniority + freshman
 formula2 <- update.formula(formula1, . ~ . + up_for_reelection)
 
+#------------------------------------------------------------------------------#
 # tab-responsiveness-regressions
+#------------------------------------------------------------------------------#
 models <- list(
   lm(formula1, house_data),
   lm(formula1, senate_data),
@@ -54,7 +104,9 @@ texreg::texreg(models, override.se = ses, override.pvalues = pvals,
   custom.coef.names = fix_coef_names(models),
   reorder.coef = c(2, 3, 16, 4:15, 1))
 
+#------------------------------------------------------------------------------#
 # tab-house-models
+#------------------------------------------------------------------------------#
 models <- list(
   lm(formula1, house_data),
   lm(formula1, house_data[dem == 1]),
@@ -83,7 +135,9 @@ texreg::texreg(models, override.se = ses, override.pvalues = pvals,
   custom.coef.names = fix_coef_names(models),
   reorder.coef = c(2:15, 1))
 
+#------------------------------------------------------------------------------#
 # tab-senate-models
+#------------------------------------------------------------------------------#
 models <- list(
   lm(formula2, senate_data),
   lm(formula2, senate_data[dem == 1]),
@@ -111,3 +165,209 @@ pvals <- mapply(function(x, y) 2 * (1 - pnorm(abs(coef(x) / y))), models, ses,
 texreg::texreg(models, override.se = ses, override.pvalues = pvals,
   custom.coef.names = fix_coef_names(models),
   reorder.coef = c(2:3, 16, 4:15, 1))
+
+#------------------------------------------------------------------------------#
+# Figure 1
+#------------------------------------------------------------------------------#
+cairo_pdf(file = "drafts/party_call_percent_both.pdf",
+  width = 6, height = 6)
+ggplot(coding_record, aes(congress, percent_party_calls,
+  color = as.factor(majority))) +
+  ylim(30, 90) +
+  xlab("Congress") +
+  ylab("Percentage of Votes") +
+  ggtitle("Frequency of Party Calls over Time") +
+  geom_point(aes(shape = as.factor(majority), size = ifelse(majority == "Democrat", 3, 2.25))) +
+  scale_shape_manual("Majority Party", values = c(16, 17), guide = FALSE) +
+  scale_color_manual("Majority Party", values = c("blue3", "red3"), guide = FALSE) +
+  scale_size_continuous(limits = c(2, 3)) +
+  # geom_smooth(method = "lm", se = FALSE) +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5),
+    text = element_text(family = "Linux Libertine"),
+    legend.position = "none") +
+  facet_wrap(~ chamber)
+dev.off()
+
+
+#------------------------------------------------------------------------------#
+# Figure 2
+#------------------------------------------------------------------------------#
+house_results <- house_data[, as.list(summary(
+    lm(formula1, data = .SD), vcov = sandwich::vcovHC(type = "HC1"))$
+    coef["ideological_extremism", 1:2]),
+  .(Congress = congress, majority)]
+senate_results <- senate_data[, as.list(summary(
+  lm(formula2, data = .SD), vcov = sandwich::vcovHC(type = "HC1"))$
+    coef["ideological_extremism", 1:2]),
+  .(Congress = congress, majority)]
+house_results[, chamber := "House"]
+senate_results[, chamber := "Senate"]
+congress_by_congress_results <- rbind(house_results, senate_results)
+
+setnames(congress_by_congress_results, "Std. Error", "SE")
+congress_by_congress_results[, q025 := Estimate + qnorm(.025) * SE]
+congress_by_congress_results[, q975 := Estimate + qnorm(.975) * SE]
+congress_by_congress_results[, q250 := Estimate + qnorm(.25) * SE]
+congress_by_congress_results[, q750 := Estimate + qnorm(.75) * SE]
+congress_by_congress_results[, maj := ifelse(majority == 1, "Majority", "Minority")]
+congress_by_congress_results[maj == "Majority", party := "Democrat"]
+congress_by_congress_results[maj == "Minority", party := "Republican"]
+congress_by_congress_results[maj == "Majority" & chamber == "House" &
+    Congress %in% house_coding_record[majority == "Republican", congress],
+  party := "Republican"]
+congress_by_congress_results[maj == "Minority" & chamber == "House" &
+    Congress %in% house_coding_record[majority == "Republican", congress],
+  party := "Democrat"]
+congress_by_congress_results[maj == "Majority" & chamber == "Senate" &
+    Congress %in% senate_coding_record[majority == "Republican", congress],
+  party := "Republican"]
+congress_by_congress_results[maj == "Minority" & chamber == "Senate" &
+    Congress %in% senate_coding_record[majority == "Republican", congress],
+  party := "Democrat"]
+
+cairo_pdf(file="drafts/both-chambers-figure2.pdf", ## RENAME
+  width = 6, height = 6)
+ggplot(congress_by_congress_results,
+  aes(Congress, Estimate, color = party, shape = party)) +
+  geom_hline(yintercept = 0, color = "gray", linetype = 3) +
+  geom_errorbar(aes(ymin = q025, ymax = q975), size = .5, width = 0) +
+  geom_errorbar(aes(ymin = q250, ymax = q750), size = 1, width = 0) +
+  geom_point(aes(size = ifelse(party == "Democrat", 2.35, 2.125))) +
+  facet_grid(maj ~ chamber, scales = "free") +
+  theme_minimal() +
+  scale_color_manual(values = c("Democrat" = "blue3", "Republican" = "red3")) +
+  scale_size_continuous(limits = c(2, 3)) +
+  ylab("Coefficient on Ideological Extremism") +
+  theme(plot.title = element_text(hjust = 0.5),
+    text = element_text(family = "Linux Libertine"),
+    strip.text = element_text(size = 12),
+    legend.position = "none")
+dev.off()
+
+#------------------------------------------------------------------------------#
+# Figure 3
+#------------------------------------------------------------------------------#
+
+# select variables needed
+DATA <- senate_data[!is.na(responsiveness_to_party_calls),
+  .(congress, x = ideological_extremism, stabb, class, caucus, majority, votes,
+    tr = up_for_reelection,
+    y1 = responsiveness_to_party_calls,
+    y2 = baseline_rate)]
+setorder(DATA, stabb, congress, class)
+
+# subset to cases with two senators, one treated, one control
+DATA <- merge(DATA,
+  DATA[, .N, .(stabb, congress)],
+  by = c("stabb", "congress"),
+  all.x = TRUE)
+DATA <- DATA[N == 2]
+DATA[, mean_tr := mean(tr), .(stabb, congress)]
+names(DATA)
+
+# Estimate Effects
+diff_responsiveness <- DATA[mean_tr == .5,
+  sum(tr * y1) - sum((1 - tr) * y1), .(stabb, congress)][,
+    mean(V1)]
+diff_baseline <- DATA[mean_tr == .5,
+  sum(tr * y2) - sum((1 - tr) * y2), .(stabb, congress)][,
+    mean(V1)]
+
+
+
+DATA[, stabb_congress := paste(stabb, congress)]
+summary(mgcv::gam(y1 ~ tr + s(x) + s(y2) + s(votes) +
+    factor(stabb_congress) +
+    caucus + majority,
+  data = DATA))
+
+summary(lfe::felm(y1 ~ tr +
+    caucus * majority |
+    stabb_congress | 0 | stabb_congress,
+  DATA))
+summary(lfe::felm(y2 ~ tr +
+    caucus * majority |
+    stabb_congress | 0 | stabb_congress,
+  DATA))
+summary(lfe::felm(x ~ tr +
+    caucus * majority |
+    stabb_congress | 0 | stabb_congress,
+  DATA))
+
+model_y <- lm(y1 ~ tr + x, DATA)
+model_m <- lm(x ~ tr, DATA)
+out <- mediation::mediate(model.m = model_m, model.y = model_y, treat = "tr", mediator = "x")
+
+
+# bootstrap by state
+states <- DATA[, unique(stabb)]
+
+boot <- function(i) {
+  boot_states <- sample(states, replace = TRUE)
+  boot_DATA <- rbindlist(lapply(seq_along(boot_states), function(boot_id) {
+    boot_DATA <- DATA[stabb == boot_states[boot_id]]
+    boot_DATA[, boot_id := boot_id]
+    boot_DATA
+  }))
+  boot_diff_responsiveness <- boot_DATA[mean_tr == .5,
+    sum(tr * y1) - sum((1 - tr) * y1), .(boot_id, congress)][,
+      mean(V1)]
+  boot_diff_baseline <- boot_DATA[mean_tr == .5,
+    sum(tr * y2) - sum((1 - tr) * y2), .(boot_id, congress)][,
+      mean(V1)]
+  data.table(boot_diff_pi, boot_diff_pf)
+}
+
+# boots <- rbindlist(lapply(1:1000, boot))
+# save(boots, file = "drafts/boots.RData")
+load("drafts/boots.RData")
+
+differences <- data.table(
+  test = c(
+    "Party Calls",
+    "Party-Free Votes"),
+  Estimate = c(
+    diff_responsiveness,
+    diff_baseline),
+  Lower_Bound = c(
+    boots[, quantile(boot_diff_responsiveness, .025)],
+    boots[, quantile(boot_diff_baseline, .025)]),
+  Upper_Bound = c(
+    boots[, quantile(boot_diff_responsiveness, .975)],
+    boots[, quantile(boot_diff_baseline, .975)]),
+  Lower_50 = c(
+    boots[, quantile(boot_diff_responsiveness, 0.25)],
+    boots[, quantile(boot_diff_baseline, 0.25)]),
+  Upper_50 = c(
+    boots[, quantile(boot_diff_responsiveness, 0.75)],
+    boots[, quantile(boot_diff_baseline, 0.75)])
+)
+
+# make coeff plot from effects
+# differences[, position := 0]
+# differences[test == "Party Free Difference", position := 1]
+#
+# differences[, test := gsub(" Difference", "", test)]
+differences[, test := factor(test, levels = test)]
+
+
+
+cairo_pdf(file="drafts/senate_difference_estimates.pdf",
+  width = 5, height = 4)
+ggplot(differences, aes(test, Estimate)) +
+  theme_minimal() +
+  geom_hline(yintercept = 0, linetype = 3, color = "gray") +
+  geom_errorbar(aes(ymin = Lower_Bound, ymax = Upper_Bound), width = 0, size = .75) +
+  geom_errorbar(aes(ymin = Lower_50, ymax = Upper_50), width = 0, size = 1.5) +
+  geom_point(size = 4) +
+  xlab("") + ylab("Reelection Difference in Same-State Senators") +
+  ggtitle(paste0("Reelection Limits Responsiveness to Party Calls")) +
+  theme(plot.title = element_text(hjust = 0.5),
+    text = element_text(family = "Linux Libertine", size = 12))
+
+dev.off()
+
+embed_fonts("drafts/senate_difference_estimates.pdf",
+  outfile = "drafts/senate_difference_estimates.pdf")
+
